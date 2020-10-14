b6451d3e87a8319873cb833310a2c516b6a9b6c6
Yathaarth Batra <yathaarthbatra10@gmail.com>
2020-10-15 02:49:01 +0530

    2nd commit

--- Arrays&Objects/index.js
+++ Arrays&Objects/index.js
@@ -15,4 +15,30 @@ console.log(arr3); //[1,2,3,4]
 var names = new Array("yathaarth", "batra", "Khatra");
 console.log(names); //["yathaarth","batra","Khatra"]
 
-//Different functions used in the array
+//Different functions and properties used in the array
+
+//-->.length-->It sets or returns the length of the array
+console.log(names.length);
+
+//-->Sort method-->Sort the elements in the array
+names.sort();
+console.log(names);
+//sort fn sorts the elements in the array acc. to ascending order or alphabetically order
+//acc. to the data types of the elements.
+
+//pop()-->This fn removes the element from the last and returns the removed element
+//push()--> This fn adds the element in the end and returns the length of the new
+
+//shift()--> This fn removes the element from the beginning and returns the removed element
+//unshift()--> This fn adds the new element in the beginning and returns the new length
+
+//reverse()--> This functions reverses the elements in the array
+
+//Array.isArray()--> This fn tells whether the array is actually an array or not and this returns bool value
+var check = Array.isArray(names);
+console.log(check);
+
+//.concat()--> This fn concatenates the two arrays
+console.log(arr3.concat(names)); //[ 1, 2, 3, 4, "Khatra", "batra", "yathaarth" ]
+
+//indexOf()--->will tell you the index of the array
